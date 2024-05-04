ca4a123e59c3248a771a64b6c7216a2ece5889fd
rishi <rishimishra7872@gmail.com>
2024-05-04 17:49:15 +0530

    Update loadmap.blade.php
    hide apis

--- resources/views/admin/properties/loadmap.blade.php
+++ resources/views/admin/properties/loadmap.blade.php
@@ -8,12 +8,7 @@
    <head>
    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDuA1HA0cE6VXwO48-VNstt7x00yz5H6tE"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDuA1HA0cE6VXwO48-VNstt7x00yz5H6tE&v=3&sensor=false&libraries=geometry"></script>
-    <script src="https://unpkg.com/measuretool-googlemaps-v3"></script>
-      
+  
       
    </head>
    
