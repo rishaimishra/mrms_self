81cdd45361f347e277010b8870e75b8759c20368
rishaimishra <rishimishra7872@gmail.com>
2024-06-04 19:45:44 +0530

    delivery dtail shift below

--- resources/views/admin/properties/assessments.blade.php
+++ resources/views/admin/properties/assessments.blade.php
@@ -1,4 +1,33 @@
 
+
+<div id="assessment" class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+        <div class="card">
+            <div class="header  bg-cyan">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h2>{{ __("Assessment Details") }}</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="body">
+                @foreach($property->assessments as $assessment)
+
+                    <div class="assessment-item">
+                        @include('admin.properties.assessment', ['assessment' => $assessment])
+                    </div>
+
+                    @if(!$loop->last)
+                        <hr/>
+                    @endif
+
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
 <div id="assessment" class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
@@ -60,34 +89,6 @@
                     @endif
 
                 @endforeach  --}}
-            </div>
-        </div>
-    </div>
-</div>
-<div id="assessment" class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-
-        <div class="card">
-            <div class="header  bg-cyan">
-                <div class="row">
-                    <div class="col-md-8">
-                        <h2>{{ __("Assessment Details") }}</h2>
-                    </div>
-                </div>
-            </div>
-
-            <div class="body">
-                @foreach($property->assessments as $assessment)
-
-                    <div class="assessment-item">
-                        @include('admin.properties.assessment', ['assessment' => $assessment])
-                    </div>
-
-                    @if(!$loop->last)
-                        <hr/>
-                    @endif
-
-                @endforeach
             </div>
         </div>
     </div>
