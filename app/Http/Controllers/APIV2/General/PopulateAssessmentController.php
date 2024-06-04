76359525ea9fde0655277435562f3de1e138b566
rishaimishra <rishimishra7872@gmail.com>
2024-06-04 17:54:29 +0530

    new value add in property use

--- app/Http/Controllers/APIV2/General/PopulateAssessmentController.php
+++ app/Http/Controllers/APIV2/General/PopulateAssessmentController.php
@@ -34,7 +34,7 @@ class PopulateAssessmentController extends ApiController
         $result['property_value_added']     = PropertyValueAdded::select('id', 'label', 'value','good_value','avg_value','bad_value')->where('is_active', 1)->get();
         $result['property_types']           = PropertyType::select('id', 'label', 'value')->where('is_active', 1)->get();
         $result['property_dimension']       = PropertyDimension::select('id', 'label', 'value')->where('is_active', 1)->get();
-        $result['property_use']             = PropertyUse::select('id', 'label', 'value')->where('is_active', 1)->get();
+        $result['property_use']             = PropertyUse::select('id', 'label', 'value')->where('is_active', 1)->orderby('order_id')->get();
         $result['property_zones']           = PropertyZones::select('id', 'label', 'value')->where('is_active', 1)->get();
         $result['swimming_pools']           = Swimming::select('id', 'label', 'value')->where('is_active', 1)->orderby('order_id')->get();
         $result['property_sanitation']      = PropertySanitationType::select('id', 'label', 'value')->where('is_active', 1)->orderby('order_id')->get();
